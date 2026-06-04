fe08c93677d4562ca827eb55d35213ffca83ab16
MauroGiaccobasso-Git <maurogiaccobasso@gmail.com>
2026-06-04 13:27:51 -0300

    feature/frontend-auth-architecture

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,78 +1,52 @@
-    import { useState } from "react";
-    import { useAuth } from "./useAuth";
+import { useState } from "react";
+import { useAuth } from "./useAuth";
 import { authApi } from "@/api/authApi";
-
 
 // Hook encargado exclusivamente
 // del flujo de autenticación.
 export function useLogin() {
-
   // Obtiene función login desde AuthProvider
   const { login } = useAuth();
 
   // Estado utilizado para indicar
   // si el login se encuentra ejecutándose.
-  const [isLoading, setIsLoading] =
-    useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Estado utilizado para almacenar
   // errores producidos durante login.
-  const [error, setError] =
-    useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   // Ejecuta proceso completo login
-  const handleLogin = async (
-    email: string,
-    password: string
-  ) => {
-
+  const handleLogin = async (email: string, password: string) => {
     setIsLoading(true);
 
     setError(null);
 
     try {
       // Solicita autenticación backend
-      const response =
-        await authApi.login(
-          email,
-          password
-          
-        );
-        console.log("Login exitoso:", response);
+      const response = await authApi.login(email, password);
 
       // Guarda sesión autenticada
-      login(
-        response.usuario,
-        response.token
-      );
+      login(response.usuario, response.token);
 
-      return response
-
+      return response;
     } catch (error) {
-
       const message =
-        error instanceof Error
-          ? error.message
-          : "Error al iniciar sesión";
+        error instanceof Error ? error.message : "Error al iniciar sesión";
 
       setError(message);
 
       return null;
-
     } finally {
-
       setIsLoading(false);
-
     }
   };
 
   return {
-
     handleLogin,
 
     isLoading,
 
     error,
-
   };
 }
